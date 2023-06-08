41a4d34ac099055490d7df58159dd5948388c6fd
Jonas Sperling <mail@jonas-sperling.de>
2023-06-08 13:10:57 +0200

    Initial commit

---
+++ backend/src/main/java/de/jonas/emotetrackerbackend/EmoteTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package de.jonas.emotetrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoteTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmoteTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/jonas/emotetrackerbackend/EmoteTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jonas.emotetrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoteTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
