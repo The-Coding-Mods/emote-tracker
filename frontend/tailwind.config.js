72ef13dbb76ea128210f5b32a5dcae7efe061647
Jonas Sperling <mail@jonas-sperling.de>
2023-07-16 16:07:15 +0200

    switch to svelte

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    colors: {
-      'background': '#1f2026',
-      'dark-primary': '#001d3d',
-      'secondary': '#003566',
-      'highlight': '#ffd60a',
-      'dark-highlight': '#ffc300'
-    },
-    textColor: {
-      'primary': '#ffffff'
-    },
     extend: {},
   },
   plugins: [],
-}
-
+};
