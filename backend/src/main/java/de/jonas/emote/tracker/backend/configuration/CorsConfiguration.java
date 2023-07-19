90f5c73b97087c3a6f749e1d99e05d21c2dc31fe
Jonas Sperling <mail@jonas-sperling.de>
2023-07-19 08:16:39 +0200

    cors config

--- backend/src/main/java/de/jonas/emote/tracker/backend/configuration/CorsConfiguration.java
+++ backend/src/main/java/de/jonas/emote/tracker/backend/configuration/CorsConfiguration.java
@@ -1,2 +1,35 @@
-package de.jonas.emote.tracker.backend.configuration;public class CorsConfig {
+package de.jonas.emote.tracker.backend.configuration;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CorsConfiguration implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // not needed
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+        throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        // not needed
+    }
 }
