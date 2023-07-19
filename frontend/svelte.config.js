690d462e0f693b82fa674c5004e7b5345e30017b
Jonas Sperling <mail@jonas-sperling.de>
2023-07-19 19:48:39 +0200

    add skeleton ui components

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,9 @@ const config = {
     }),
   ],
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: "200.html"
+    }),
   },
 };
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: "class",
+    content: [
+        "./src/**/*.{html,js,svelte,ts}",
+        require('path')
+            .join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte,ts}")],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        ...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
+    ],
 };
