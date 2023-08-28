6fae01946027c5fe914ad1e3a7dc70f3432f5512
Jonas Sperling <mail@jonas-sperling.de>
2023-08-28 13:54:26 +0200

    start adding routing

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -11,9 +11,15 @@
     );
 </script>
 
-<header class="bg-lime-600 flex justify-between mb-5">
-    <h1 class="text-4xl p-4 justify-center">Emote Tracker</h1>
-    <div class="text-4xl p-4 align-middle">
-        {formatter.format($time)}
+
+<nav class="bg-lime-600 fixed top-0 w-[100%] max-h-20 min-h-20">
+    <div class="flex ms-2 h-20">
+        <a href="/"><h1 class="text-4xl p-4 font-bold">Emote Tracker</h1></a>
+        <div class="flex">
+            <a href="/users"><h1 class="text-2xl p-4 uppercase">Users</h1></a>
+            <div class="text-4xl p-4">
+                {formatter.format($time)}
+            </div>
+        </div>
     </div>
-</header>
+</nav>
