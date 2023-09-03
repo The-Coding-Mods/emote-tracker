fc6163a0caae8f8ec0137fe8aecc1854a42c308e
Jonas Sperling <mail@jonas-sperling.de>
2023-09-03 20:18:25 +0200

    routing

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -9,7 +9,13 @@
     // Finally, your application's global stylesheet (sometimes labeled 'app.css')
     import '../app.css';
     import Header from "$lib/components/Header.svelte";
+    import { AppShell } from "@skeletonlabs/skeleton";
 </script>
+<AppShell>
+    <svelte:fragment slot="header">
+        <Header/>
+    </svelte:fragment>
 
-<Header/>
-<slot/>
+
+    <slot/>
+</AppShell>
--- frontend/src/routes/users/[id]/+layout.svelte
+++ frontend/src/routes/users/[id]/+layout.svelte
@@ -1,12 +1,1 @@
-<script>
-    import TopList from "$lib/components/TopList.svelte";
-    import Usage from "$lib/components/Usage.svelte";
-</script>
-
-<div class="flex justify-evenly">
-    <TopList isTop={true} count={10}/>
-    <TopList isTop={false} count={10}/>
-</div>
-<Usage/>
-
 <slot/>
