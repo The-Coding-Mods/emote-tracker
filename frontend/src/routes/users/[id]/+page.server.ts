68921e05dc09d84984a88cdf4b524c2ee4bae808
codefactor-io <support@codefactor.io>
2023-09-08 15:18:54 +0000

    [CodeFactor] Apply fixes

--- frontend/src/routes/users/[id]/+page.server.ts
+++ frontend/src/routes/users/[id]/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types';
 
 const userApi = new UserApi(new Configuration({basePath: "http://localhost:8080"}));
 export const load: PageServerLoad = async ({ params }) => {
-    let topEmotes = await userApi.getTopEmotes({userId: "40646018", count: 50});
-    let bottomEmotes = await userApi.getBottomEmotes({userId: "40646018", count: 50});
+    const topEmotes = await userApi.getTopEmotes({userId: "40646018", count: 50});
+    const bottomEmotes = await userApi.getBottomEmotes({userId: "40646018", count: 50});
     return {topEmotes, bottomEmotes}
 }
