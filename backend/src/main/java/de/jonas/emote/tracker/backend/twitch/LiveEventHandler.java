50240385fbf96891dc1fe65c3ba736f050962d3b
Jonas Sperling <mail@jonas-sperling.de>
2023-11-04 15:43:50 +0100

    change to userservice in liveevent handler

--- backend/src/main/java/de/jonas/emote/tracker/backend/twitch/LiveEventHandler.java
+++ backend/src/main/java/de/jonas/emote/tracker/backend/twitch/LiveEventHandler.java
@@ -1,21 +1,23 @@
 package de.jonas.emote.tracker.backend.twitch;
 
 import com.github.twitch4j.events.ChannelGoLiveEvent;
+import de.jonas.emote.tracker.backend.user.UserService;
 import java.util.function.Consumer;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LiveEventHandler implements Consumer<ChannelGoLiveEvent> {
 
-    private final EmoteUpdateService updateService;
+    private final UserService userService;
 
-    public LiveEventHandler(EmoteUpdateService updateService) {
-        this.updateService = updateService;
+    public LiveEventHandler(UserService userService) {
+        this.userService = userService;
     }
+
 
     @Override
     public void accept(ChannelGoLiveEvent channelGoLiveEvent) {
         String userId = channelGoLiveEvent.getChannel().getId();
-        updateService.updateUserEmotes(userId);
+        userService.updateEmotes(userId);
     }
 }
