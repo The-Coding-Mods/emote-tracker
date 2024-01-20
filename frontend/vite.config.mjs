0839ef2387cf20b4909a41611c0b0f2062ae5a36
Jonas Sperling <mail@jonas-sperling.de>
2024-01-20 21:27:50 +0100

    load environment variables in vite config

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,8 +1,16 @@
+import { defineConfig, loadEnv } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
-const config = {
-    plugins: [sveltekit()],
+export default ({mode}) => {
+    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+    return defineConfig({
+        plugins: [sveltekit()],
+        server: {
+            proxy: {
+                "/api": process.env.BACKEND_URL,
+            }
+        },
+    })
 };
 
-export default config;
