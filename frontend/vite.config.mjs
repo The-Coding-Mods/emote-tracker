6db9c1527708c504cddf523d441525e86e843b23
Jonas Sperling <mail@jonas-sperling.de>
2024-01-20 23:54:06 +0100

    Revert "load environment variables in vite config"
    This reverts commit 0839ef2387cf20b4909a41611c0b0f2062ae5a36.

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,16 +1,8 @@
-import { defineConfig, loadEnv } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
-export default ({mode}) => {
-    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
-    return defineConfig({
-        plugins: [sveltekit()],
-        server: {
-            proxy: {
-                "/api": process.env.BACKEND_URL,
-            }
-        },
-    })
+const config = {
+    plugins: [sveltekit()],
 };
 
+export default config;
