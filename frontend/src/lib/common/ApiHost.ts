ec43c8e950abed21ad502acad1c1692eb2326b1a
Jonas Sperling <mail@jonas-sperling.de>
2024-04-27 14:29:59 +0200

    test

--- frontend/src/lib/common/ApiHost.ts
+++ frontend/src/lib/common/ApiHost.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "/backend"; // use http://localhost:8080 for local development
+export const BACKEND_URL = "http://emote-tracker-backend:8080/"; // use http://localhost:8080 for local development
