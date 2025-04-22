0f0700f4553e899201bf3751544a8d85ca77a7f7
Jonas Sperling <mail@jonas-sperling.de>
2025-04-22 18:39:25 +0200

    add index to emote

--- backend/src/main/java/de/jonas/emote/tracker/backend/database/Emote.java
+++ backend/src/main/java/de/jonas/emote/tracker/backend/database/Emote.java
@@ -1,8 +1,7 @@
 package de.jonas.emote.tracker.backend.database;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
+import jakarta.persistence.*;
+
 import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +12,7 @@ import lombok.experimental.Accessors;
 @Setter
 @Accessors(chain = true)
 @IdClass(EmoteId.class)
+@Table(indexes = {@Index(name = "idx_emote_id", columnList = "id"), @Index(name = "idx_emote_name", columnList = "name")})
 public class Emote {
     @Id
     protected String id;
